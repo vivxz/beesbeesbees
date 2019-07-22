39ab38434f59f8b49e0b47c468d02d40e189da2e
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-07-22 12:08:27 -0700

    Completed BMR

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this, arguments);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing"
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
